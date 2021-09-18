cf4302f86b68c29296ed0dad61cb0a7083ad66cc
EC2 Default User <ec2-user@ip-172-31-30-243.ec2.internal>
2021-09-18 00:34:27 +0000

    first commit

---
+++ warmup/for_while.py
@@ -0,0 +1,10 @@
+# for i in range(10):
+#     print(i, end="/")
+
+total = 0
+i = 0
+total=1,3,6,10...
+while(total <=50):
+    i += 1
+    total += i
+print(i)
---
+++ warmup/if.py
@@ -0,0 +1,19 @@
+# if
+age = int(input("何歳ですか？"))
+if age >= 20:
+    print("you can drink alcohol!!")
+else:
+    print("you can't drink alcohol")
+    
+# if elif else
+score = int(input("テストの点数を教えて"))
+if score >= 85:
+    print("S")
+elif score >= 75:
+    print("A")
+elif score >= 65:
+    print("B")
+elif score >= 55:
+    print("C")
+else:
+    print("D")
